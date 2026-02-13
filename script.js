bd685e1520ef1c16b1c2df469e812f96388d5031
Fola-luke1 <folakanoyeluke@gmail.com>
2026-02-13 22:07:44 +0100

    divide function english correction

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 
 const divide = (a, b) => {
     if (b === 0) {
-        return "Cannot divide by zero";
+        return "Cannot be divided by zero";
     }
     return a / b;
 };
-
 function Calculator() {
    
     console.log(`divide = ${divide(10, 2)}`);
     
 }
 Calculator();
-
 module.exports = { divide };
