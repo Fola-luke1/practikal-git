4b49eb0ce7fa2db68902bcd98c6b757ccf673952
Fola-luke1 <folakanoyeluke@gmail.com>
2026-02-13 18:25:50 +0100

    subtract function

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-const add = (a, b) => {
-    return a + b;
-}
+
 
 const subtract = (a, b) => {
     return a - b;
 }
 
-const multiply = (a, b) => {
-    return a * b;
-}
-
-
-const divide = (a, b) => {
-    if (b === 0) {
-        return "Cannot divide by zero";
-    }
-    return a / b;
-};
-
-function Calculator() {
-    console.log(`add = ${add(10, 5)}`);
-    console.log(`subtract = ${subtract(10, 5)}`);
-    console.log(`multiply = ${multiply(10, 5)}`);
-    console.log(`divide = ${divide(10, 5)}`);
-    
+function Calculator() {   
+    console.log(`subtract = ${subtract(10, 2)}`);   
 }
 Calculator();
 
-module.exports = { add, subtract, multiply, divide };
+module.exports = {subtract };
