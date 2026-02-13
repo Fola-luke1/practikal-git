711ea5cde36c32d797a0dd6c15f548e512b02037
Fola-luke1 <folakanoyeluke@gmail.com>
2026-02-13 18:13:20 +0100

    feat add function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const add = (a, b) => {
     return a + b;
 }
 function Calculator() {
-    console.log(`add = ${add(10, 5)}`);
+    console.log(`add = ${add(10, 2)}`);
 }
 Calculator();
 module.exports = { add};
