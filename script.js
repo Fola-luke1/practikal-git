a6938d49a401600ff0020f43e26ea64a0b5ed463
Fola-luke1 <folakanoyeluke@gmail.com>
2026-02-15 13:37:05 +0100

    making some subtraction adjustment in subtarction function

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
-
 const subtract = (a, b) => {
     return a - b;
 }
-
 function Calculator() {   
     console.log(`subtract = ${subtract(10, 2)}`);   
 }
 Calculator();
 
-module.exports = {subtract };
+module.exports = {subtract};
