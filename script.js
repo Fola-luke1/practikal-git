9fddd0b667993bd677cc3af145f95f83ecdefd88
Fola-luke1 <folakanoyeluke@gmail.com>
2026-02-25 17:47:41 +0100

    new implementation in subtraction

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const subtract = (a, b) => {
     return a - b;
 }
-function Calculator() {   
-    console.log(`subtract = ${subtract(10, 2)}`);   
-}
-Calculator();
+
 module.exports = {subtract}; 
+
