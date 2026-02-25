2871463d2e1453c57ad280607a8684199d3ce11d
Fola-luke1 <folakanoyeluke@gmail.com>
2026-02-25 17:55:59 +0100

    new implemtation in multiple

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const multiply = (a, b) => {
     return a * b;
 }
-function Calculator() {
-    console.log(`multiply = ${multiply(10, 2)}`);    
-}
-Calculator();
 
 module.exports = { multiply };
